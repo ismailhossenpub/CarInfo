8dfed401f029e160112450d65558ff31e20d0e50
Saad Hasan <saadh393@gmail.com>
2021-06-03 16:33:37 +0600

    Butttons Are Ready

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import BottomButton from "./Components/Buttons/BottomButton";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <BottomButton
+        type="primary"
+        content={"Button One"}
+        onPress={() => {
+          console.log("Custom Order was pressed");
+        }}
+      />
+
+      <BottomButton
+        type="secondary"
+        content={"Button Two"}
+        onPress={() => {
+          console.log("Existing Inventory was pressed");
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
